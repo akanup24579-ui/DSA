413daf1d3d389c563224b1cf8b2feb18987927d5
Ajay <akanup24579@gmail.com>
2026-07-14 18:48:42 +0530

    Added Single Number Code using BitManipulation

---
+++ src/DSA/SinglenumberbitManipulatiob.java
@@ -0,0 +1,13 @@
+package DSA;
+
+public class SinglenumberbitManipulatiob {
+    public static void main(String[] args) {
+    int []arr= {4,1,2,1,2};
+    int result=0;
+    for(int i=0; i<arr.length;i++){
+        result=result^arr[i];
+    }
+    System.out.println(result);
+    }
+
+}
